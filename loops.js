b232e3b5b7334b0bc38bac51997aa5d68c8eb3e2
Damian Malysza <damian.malysza@gmail.com>
2020-09-21 01:38:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,15 +16,17 @@ function whileLoop(num){
   return "done"
 }
 
-let i=0
 
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
 
 function doWhileLoop(num){
   do{
     console.log("I run once regardless.")
   } while (incrementVariable()<num)
+}
+
+let i=0
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
 }
