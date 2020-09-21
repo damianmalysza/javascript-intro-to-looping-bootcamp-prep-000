04273c0f715dfdc087cb841ef9fd7dc3dbdaca03
Damian Malysza <damian.malysza@gmail.com>
2020-09-21 01:43:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -25,7 +25,7 @@ function incrementVariable() {
 
 function doWhileLoop(num){
   do{
-    console.log("I run once regardless."+i);
+    console.log("I run once regardless.");
   } while (incrementVariable()<num);
   i=0
 }
